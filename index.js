1388516d44e70f195306ade7933bbcf30376311b
Diamant Lelouette <git@diamant.dev>
2023-04-01 19:19:57 +0200

    Meow meow

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const meowb = document.querySelector('.meowb');
 const meow = document.querySelector('.meow');
 
 meowb.addEventListener('click', () => {
-  // Replace every word with "meow" and add "😺 Pinky." at the end.
-  meow.textContent = meow.textContent.replace(/\w+/g, 'meow') + '😺 Pinky.';
+  // Replace every word with "meow" and add "😺 Pinky." at the end. (Note: meow is a textArea)
+  meow.value = meow.value.replace(/\w+/g, 'meow') + '😺 Pinky.';
 });
